f6f00b5bb07f0fa966ed267dd73be44614fd6704
thriveda <thriveda666@gmail.com>
2025-09-18 12:30:57 +0530

    fourth Commit

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,18 @@ dotenv.config()
 app.use(cors({
   origin: "http://localhost:5173",   
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 
-app.options("*", cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200) 
+  }
+  next()
+})
 
 app.use(express.json())
 app.use(bodyParser.json())
